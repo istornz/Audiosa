0ae5cc60c2e6cc08539dd7ad0bb78ba21fd75953
kev-sx <kevin.chansonl2a38@gmail.com>
2016-01-21 16:42:04 +0100

    Update main.cpp
    Ajout test modification volume

--- lecteur/main.cpp
+++ lecteur/main.cpp
@@ -17,17 +17,28 @@ int main(int argc, const char** argv)
 
   engine->play2D("D:\\Dropbox\\Projet_Lecteur\\media\\08_Iron_Hand.flac"); // Lancement de la musique
 
+  // Test Modification du Volume
+  engine->setSoundVolume(0.5);                                             //Met le volume a 0.5
+  std::cin >> i;                                                           //Attend que l'utilisateur appuie sur une touche
+  engine->setSoundVolume(0);                                               //Met le volume a 0
+  std::cin >> i;                                                           //Attend que l'utilisateur appuie sur une touche
+  engine->setSoundVolume(0.5);                                             //Met le volume a 0.5
+  std::cin >> i;                                                           //Attend que l'utilisateur appuie sur une touche
+  engine->setSoundVolume(1);                                               //Met le volume a 1
+
+  // Test Mise en Route et Play
   std::cin >> i;                                                           //Attend que l'utilisateur appuie sur une touche
   engine->setAllSoundsPaused(true);                                        //Met la musique en pause
   
   std::cin >> i;                                                           //Attend que l'utilisateur appuie sur une touche
   engine->setAllSoundsPaused(false);                                       //Remet la musique en route
   
+  // Test Changement de Musique
   std::cin >> i;                                                           //Attend que l'utilisateur appuie sur une touche
   engine->removeAllSoundSources();                                         //Supprime les musiques qui sont dans l'objet engine
   engine->play2D("D:\\Dropbox\\Projet_Lecteur\\media\\11_Tourettes.flac", true); // Lancement de la musique en boucle (true)
 
-
+  // Fermer Programme
   std::cin >> i; // wait for user to press a key
   engine->drop(); // delete engine
 

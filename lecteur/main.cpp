9125fecaa60d738990c47104967abc173a411da6
kev-sx <kevin.chansonl2a38@gmail.com>
2016-01-21 16:22:32 +0100

    Update main.cpp
    Lancement d'une musique, mise en pause, remise en route. Puis changement de musique en vidant avant le contenu de l'objet engine.

--- lecteur/main.cpp
+++ lecteur/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <irrKlang.h>
+#include <windows.h>
 
 using namespace irrklang;
 
@@ -10,13 +11,24 @@ int main(int argc, const char** argv)
 
   if (!engine)
     return 0; // error starting up the engine
+  
+   char i = 0;
+  
 
-  // play some sound stream, looped
-  engine->play2D("D:\\Dropbox\\Projet_Lecteur\\media\\11_Tourettes.flac", true);
+  engine->play2D("D:\\Dropbox\\Projet_Lecteur\\media\\08_Iron_Hand.flac"); // Lancement de la musique
 
-  char i = 0;
+  std::cin >> i;                                                           //Attend que l'utilisateur appuie sur une touche
+  engine->setAllSoundsPaused(true);                                        //Met la musique en pause
+  
+  std::cin >> i;                                                           //Attend que l'utilisateur appuie sur une touche
+  engine->setAllSoundsPaused(false);                                       //Remet la musique en route
+  
+  std::cin >> i;                                                           //Attend que l'utilisateur appuie sur une touche
+  engine->removeAllSoundSources();                                         //Supprime les musiques qui sont dans l'objet engine
+  engine->play2D("D:\\Dropbox\\Projet_Lecteur\\media\\11_Tourettes.flac", true); // Lancement de la musique en boucle (true)
+
+
   std::cin >> i; // wait for user to press a key
-
   engine->drop(); // delete engine
 
   return 0;

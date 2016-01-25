fa54ee2e105e3bd6b7f24e208f507a33e75c3dd0
kev-sx <kevin.chansonl2a38@gmail.com>
2016-01-25 14:43:40 +0100

    Messages
    Ajout des cout

--- lecteur/main.cpp
+++ lecteur/main.cpp
@@ -1,8 +1,3 @@
-//Test
-//Chanson Kévin
-//V0.1
-//21/01/2016
-//17h12
 #include <iostream>
 #include <irrKlang.h>
 #include <windows.h>
@@ -16,31 +11,39 @@ int main(int argc, const char** argv)
 
   if (!engine)
     return 0; // error starting up the engine
-  
+
    char i = 0;
-  
+
 
   engine->play2D("D:\\Dropbox\\Projet_Lecteur\\media\\08_Iron_Hand.flac"); // Lancement de la musique
-
+  std::cout << "Lancement de la musique Iron Hand" << std::endl;
   // Test Modification du Volume
+  std::cin >> i;
+  std::cout << "Volume a 0.5" << std::endl;
   engine->setSoundVolume(0.5);                                             //Met le volume a 0.5
   std::cin >> i;                                                           //Attend que l'utilisateur appuie sur une touche
+  std::cout << "Volume a 0" << std::endl;
   engine->setSoundVolume(0);                                               //Met le volume a 0
   std::cin >> i;                                                           //Attend que l'utilisateur appuie sur une touche
+  std::cout << "Volume a 0.5" << std::endl;
   engine->setSoundVolume(0.5);                                             //Met le volume a 0.5
   std::cin >> i;                                                           //Attend que l'utilisateur appuie sur une touche
+  std::cout << "Volume a 1" << std::endl;
   engine->setSoundVolume(1);                                               //Met le volume a 1
 
   // Test Mise en Pause et Play
   std::cin >> i;                                                           //Attend que l'utilisateur appuie sur une touche
+  std::cout << "Musique en pause" << std::endl;
   engine->setAllSoundsPaused(true);                                        //Met la musique en pause
-  
+
   std::cin >> i;                                                           //Attend que l'utilisateur appuie sur une touche
+  std::cout << "Play musique" << std::endl;
   engine->setAllSoundsPaused(false);                                       //Remet la musique en route
-  
+
   // Test Changement de Musique
   std::cin >> i;                                                           //Attend que l'utilisateur appuie sur une touche
   engine->removeAllSoundSources();                                         //Supprime les musiques qui sont dans l'objet engine
+  std::cout << "Changement de musique - Play Tourette's de Nirvana" << std::endl;
   engine->play2D("D:\\Dropbox\\Projet_Lecteur\\media\\11_Tourettes.flac", true); // Lancement de la musique en boucle (true)
 
   // Fermer Programme
@@ -49,4 +52,6 @@ int main(int argc, const char** argv)
 
   return 0;
 }
+
+
 

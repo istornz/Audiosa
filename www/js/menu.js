cbe3c8101838ced892d30c4aeb1ea00349842b3f
Lkaizer <MOHAMED_BEN@H115-GM-PC17.lgm.local>
2016-02-08 11:08:20 +0100

    biz

--- www/js/menu.js
+++ www/js/menu.js
@@ -1,6 +1,42 @@
 (function($){
 	$(window).load(function(){
-	var page_actuel = "morceaux";
+		var page_actuel = 1; //1: morceaux / 2: artist / 3: album
 	
+		//Morceaux
+		$(".menu_morceaux").click(function() {
+			if(page_actuel == 1) {
+				//Ne rien faire
+			}
+			else
+			{
+				$("#mCSB_1_container").html("");
+			}
+		});
+		
+		//Aristes
+		$(".menu_artist").click(function() {
+			if(page_actuel == 2) {
+				//Ne rien faire
+			}
+			else
+			{
+				$("#mCSB_1_container").html("");
+			}
+		});
+		
+		//Album
+		$(".menu_album").click(function() {
+			if(page_actuel == 3) {
+				//Ne rien faire
+			}
+			else
+			{
+				$("#mCSB_1_container").html("");
+			}
+		});
 	});
 })(jQuery);
+
+function testaa() {
+	alert("cc");
+}

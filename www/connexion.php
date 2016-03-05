32ed460f8ed2f6f8b28692c535fca7d1a905450c
istornz <DESSUS1@H115-GM-PC10.lgm.local>
2016-03-05 10:38:51 +0100

    er

--- www/connexion.php
+++ www/connexion.php
@@ -6,7 +6,7 @@ $USER_LOGIN 	= "db_reader_user";
 $USER_PSW		= "VpuCdvqjwNU3ce5T";
 
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=test', $USER_LOGIN, $USER_PSW);
+    $dbh = new PDO('mysql:host=localhost;dbname=audio_db', $USER_LOGIN, $USER_PSW);
     foreach($dbh->query('SELECT * from UTILISATEURS') as $row) {
         print_r($row);
     }

834a1bb687c403bc7c4788d63fbadf67a5f63fba
istornz <DESSUS1@H115-GM-PC10.lgm.local>
2016-03-05 10:37:40 +0100

    df

--- www/connexion.php
+++ www/connexion.php
@@ -5,11 +5,15 @@ $DB_NAME	 	= "audio_db";
 $USER_LOGIN 	= "db_reader_user";
 $USER_PSW		= "VpuCdvqjwNU3ce5T";
 
-$link = mysql_connect($DB_HOST, $USER_LOGIN, $USER_PSW);
-if (!$link) {
-    die('Connexion impossible : ' . mysql_error());
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=test', $USER_LOGIN, $USER_PSW);
+    foreach($dbh->query('SELECT * from UTILISATEURS') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
 }
-echo 'Connecté correctement';
-mysql_close($link);
 
 ?>

b219cd3f658b1f566992dcee8c297733336bce35
istornz <DESSUS1@lgm.local>
2016-03-31 17:10:36 +0200

    pls

--- www/apis/conf.php
+++ www/apis/conf.php
@@ -3,8 +3,11 @@
 $DB_HOST	 	= "localhost";
 $DB_NAME	 	= "audio_db";
 
-$DB_READER_LOGIN 	= "db_reader_user";
-$DB_READER_PSW		= "VpuCdvqjwNU3ce5T";
+$DB_READER_USER_LOGIN 	= "db_reader_user";
+$DB_READER_USER_PSW		= "VpuCdvqjwNU3ce5T";
+
+$DB_READER_MUSIC_LOGIN 	= "db_reader_music";
+$DB_READER_MUSIC_PSW	= "qsZuJe6hruAm9nBs";
 
 $DB_WRITER_LOGIN 	= "db_writer_music";
 $DB_WRITER_PSW		= "5VHKUmhKrsSspmjs";

8f7887eb3a40db435a1eb5e52a42a5d1bdf49519
Lkaizer <MOHAMED_BEN@lgm.local>
2016-04-01 10:46:18 +0200

    conf

--- www/apis/conf.php
+++ www/apis/conf.php
@@ -3,10 +3,10 @@
 $DB_HOST	 	= "localhost";
 $DB_NAME	 	= "audio_db";
 
-$DB_READER_LOGIN 	= "db_reader_user";
-$DB_READER_PSW		= "VpuCdvqjwNU3ce5T";
+$DB_READER_LOGIN 	= "root";
+$DB_READER_PSW		= "";
 
-$DB_WRITER_LOGIN 	= "db_writer_music";
-$DB_WRITER_PSW		= "5VHKUmhKrsSspmjs";
+$DB_WRITER_LOGIN 	= "root";
+$DB_WRITER_PSW		= "";
 
 ?>

668198f875add114b584c1448cc6c864bf709a7a
istornz <Dimitri@macbook-pro-de-dimitri.home>
2016-05-05 18:13:57 +0200

    PLS DOREE

--- www/js/client/client.js
+++ www/js/client/client.js
@@ -1,25 +1,34 @@
 ﻿// Connexion à socket.io
 console.log("CLIENT socket lancé:");
 
-isPlayed	= false;
-isPaused	= false;
-emplacement = 0; //0: pistes; 1: album; 2: playlist
-idMusic		= 10;
-volumeRate 	= 0.5;
-mode_player = 0 //Lecture a la suite, 1 pour alea
+isPlayed			= false;
+isPaused			= false;
+emplacement 		= 2; //0: pistes; 1: album; 2: playlist
+emplacement_name 	= "14"; //Nom album ou idPlaylist
+idMusic				= 53;
+volumeRate 			= 1;
+mode_player 		= 0; //Lecture a la suite, 1 pour alea
 
-wsocket = new WebSocket("ws://172.16.126.170:8081");	
+wsocket = new WebSocket("ws://192.168.1.15:8081");	
 wsocket.onopen = function()
 {
 	console.log("Connected !");
 };
 
 wsocket.onmessage = function (evt) 
-{ 
+{
 	var received_msg = evt.data;
-	var jsonCallback = JSON.parse(received_msg);
-	idMusic = jsonCallback.id_music_played;
-	console.log(idMusic);
+	console.log(received_msg);
+	
+	try {
+        var jsonCallback = JSON.parse(received_msg);
+        idMusic = jsonCallback.id_music_played;
+    } catch (e) {
+	    console.log(e);
+        return false;
+    }
+    
+    console.log(idMusic);
 };
 
 wsocket.onclose = function()
@@ -29,14 +38,14 @@ wsocket.onclose = function()
 
 $("#web-player-previous").click(function() {
 	
-	wsocket.send('{ "player_mode": '+mode_player+', "playlist": {"mode": 1,	"id_playlist": 47  }, "id_music": '+idMusic+',  "action": 4, "volume_rate": '+ volumeRate +', "emplacement": '+emplacement+' }');				  
+	wsocket.send('{"id_music": '+idMusic+',"action": 4,"player_mode": '+mode_player+',"volume_rate": '+ volumeRate +',"emplacement_mode": '+emplacement+',"emplacement_name": "'+emplacement_name+'"}');				  
 
 });
 
 
 $("#web-player-next").click(function() {
 
-	wsocket.send('{ "player_mode": '+mode_player+', "playlist": {"mode": 1,	"id_playlist": 47  }, "id_music": '+idMusic+',  "action": 3, "volume_rate": '+ volumeRate +', "emplacement": '+emplacement+' }');				  
+	wsocket.send('{"id_music": '+idMusic+',"action": 3,"player_mode": '+mode_player+',"volume_rate": '+ volumeRate +',"emplacement_mode": '+emplacement+',"emplacement_name": "'+emplacement_name+'"}');					  
 
 });
 
@@ -56,18 +65,18 @@ $("#web-player-play").click(function() {
 		if(isPaused)
 		{
 			isPaused = false;
-			wsocket.send('{ "player_mode": '+mode_player+', "playlist": {"mode": 1,	"id_playlist": 47  }, "id_music": '+idMusic+',  "action": 2, "volume_rate": '+ volumeRate +', "emplacement": '+emplacement+' }');				  
+			wsocket.send('{"id_music": '+idMusic+',"action": 2,"player_mode": '+mode_player+',"volume_rate": '+ volumeRate +',"emplacement_mode": '+emplacement+',"emplacement_name": "'+emplacement_name+'"}');					  
 		}
 		else
 		{
 			isPaused = true;
-			wsocket.send('{ "player_mode": '+mode_player+', "playlist": {"mode": 1,	"id_playlist": 47  }, "id_music": '+idMusic+',  "action": 1, "volume_rate": '+ volumeRate +', "emplacement": '+emplacement+' }');				  
+			wsocket.send('{"id_music": '+idMusic+',"action": 1,"player_mode": '+mode_player+',"volume_rate": '+ volumeRate +',"emplacement_mode": '+emplacement+',"emplacement_name": "'+emplacement_name+'"}');				  
 		}
 	}
 	else
 	{
 		isPlayed = true;
-		wsocket.send('{ "player_mode": '+mode_player+', "playlist": {"mode": 1,	"id_playlist": 47  }, "id_music": '+idMusic+',  "action": 0, "volume_rate": '+ volumeRate +', "emplacement": '+emplacement+' }');				  
+		wsocket.send('{"id_music": '+idMusic+',"action": 0,"player_mode": '+mode_player+',"volume_rate": '+ volumeRate +',"emplacement_mode": '+emplacement+',"emplacement_name": "'+emplacement_name+'"}');		  
 	}
 		
 });
